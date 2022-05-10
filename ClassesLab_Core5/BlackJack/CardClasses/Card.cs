a8a4550fb180ddb749300e645f756ed11568b541
LunarMoo <102759790+LunarMoo@users.noreply.github.com>
2022-05-10 09:48:37 -0700

    CardClass updated

--- ClassesLab_Core5/BlackJack/CardClasses/Card.cs
+++ ClassesLab_Core5/BlackJack/CardClasses/Card.cs
@@ -11,6 +11,137 @@ namespace CardClasses
         private int value;
         private int suit;
 
+        public Card()
+        {
+            value = 1;
+            suit = 1;
+        }
+
+        public Card(int v, int s)
+        {
+            value = v;
+            suit = s;
+        }
+
+        public int Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                if (this.value <= 13 || this.value >= 1)
+                    this.value = value;
+                else
+                    throw new ArgumentException("Value must be between 1 and 13");
+
+            }
+        }
+
+        public int Suit
+        {
+            get
+            {
+                return suit;
+            }
+            set
+            {
+                if (value <= 4 || value >= 1)
+                    this.suit = value;
+                else
+                    throw new ArgumentException("Value must be between 1 and 4");
+            }
+        }
+        public bool HasMatchingSuit(Card other)
+        {
+            if (this.suit == other.suit)
+                return true;
+            else
+                return false;
+        }
+        public bool HasMatchingValue(Card other)
+        {
+            if (this.value == other.suit)
+                return true;
+            else
+                return false;
+        }
+        public bool IsAce()
+        {
+            if (value == 1)
+                return true;
+
+            else
+                return false;
+        }
+        public bool IsBlack()
+        {
+            bool black;
+            if (suit == 1)
+            {
+                black = true;
+                return black;
+            }
+            else
+                return false;
+        }
+        public bool IsClub()
+        {
+            if (suit == 1)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool IsDiamond()
+        {
+            if (suit == 1)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool IsFaceCard()
+        {
+            if (suit == 1)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool IsHeart()
+        {
+            if (suit == 1)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public bool IsRed()
+        {
+            bool red;
+            if (suit == 1)
+            {
+                red = true;
+                return red;
+            }
+            else
+                return false;
+        }
+        public bool IsSpade()
+        {
+            if (suit == 1)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
         public override string ToString()
         {
             return values[value] + " of " + suits[suit];
