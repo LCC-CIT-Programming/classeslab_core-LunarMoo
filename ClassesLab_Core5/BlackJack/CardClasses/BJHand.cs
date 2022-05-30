3a6f02ee3582a28c905c7e34ea9eb546c49987bd
LunarMoo <102759790+LunarMoo@users.noreply.github.com>
2022-05-29 23:21:06 -0700

    Uploaded BJHand code

---
+++ ClassesLab_Core5/BlackJack/CardClasses/BJHand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace CardClasses
+{
+    public class BJHand : Hand
+    {
+        public BJHand() : base() { }
+
+        public BJHand(Deck d, int numCards) : base(d, numCards) { }
+
+        public bool HasAce
+        {
+            get
+            {
+                return true; // HasCard(1)
+                
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                foreach (Card c in cards)
+                {
+                    if (c.IsFaceCard())
+                        score += 10;
+                    else
+                        score += c.Value;
+                }
+                if (this.HasAce && score <= 11)
+                    score += 10;
+                return 0;
+            }
+        }
+        public bool IsBusted
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
